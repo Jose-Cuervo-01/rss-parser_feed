8d97e86f1d05b4b320ca3b8238250d0ecd03cea8
Jose-Cuervo-01 <jocuervo007@gmail.com>
2020-03-30 14:00:00 -0600

    ok

---
+++ routes/podcast.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+//const parser = require('rss-url-parser');
+
+let Parser = require('rss-parser');
+let par = new Parser();
+
+/* GET podcast page. */
+router.get('/', function(req, res, next) {
+    res.render('podcast');
+  });
+  module.exports = router;
+
+
+(async () => {
+ 
+    let feed = await par.parseURL('http://feeds.soundcloud.com/users/soundcloud:users:801063742/sounds.rss');
+    //console.log(feed.title);
+    feed.items.forEach(item => {
+      console.log(feed);
+    });
+   
+  })();
+  //parser('http://feeds.soundcloud.com/users/soundcloud:users:293803449/sounds.rss').then((data) => console.log(data))
+  
